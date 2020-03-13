1815d64004bd45ac044e805c6b36020a8efb4c8c
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-12 18:02:17 -0700

    Add project files.

---
+++ Assignment 221/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = new DateTime();
+            time = DateTime.Now;
+            Console.WriteLine(time);
+            Console.WriteLine("Please enter a number:");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            DateTime newTime = time.AddHours(hours);
+            Console.WriteLine("In " + hours + " hours, it will be " + newTime);
+            Console.ReadLine();
+
+        }
+    }
+}
